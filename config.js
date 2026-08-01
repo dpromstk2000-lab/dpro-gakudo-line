1270b54a385d5e11c2cecae3148d64cd1fda5f37
dpromstk2000-lab <dpromstk2000@gmail.com>
2026-08-01 13:52:30 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+window.DPRO_GAKUDO_CONFIG = Object.freeze({
+  apiBase: "https://dpro-gakudo-line-api.dpromstk2000.workers.dev",
+  facilityCode: "dpro_gakudo_demo",
+  facilityName: "DPRO 学童保育クラブ（DEMO）",
+  version: "GAKUDO-4-FRONT-20260801",
+  demo: { lineUserId: "demo_gakudo_line_001", phone: "090-4800-2001" }
+});
